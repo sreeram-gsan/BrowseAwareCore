2dc717008d2b527f20b4f6062f82e683bfbb3585
sreeram-gsan <sreeram2910@gmail.com>
2020-06-01 20:43:25 +0000

    Added setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="BrowseAwareCore", 
+    version="20.0.4",
+    author="Sreeram Ganesan",
+    author_email="sreeram2910@gmail.com",
+    description="A package to extract, pre-process and manipulate data from the URLs and to scientifically predict distractions while browsing.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sreeram-gsan/BrowseAwareCore",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
